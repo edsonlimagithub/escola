b57fcd919a80ba1554d559900db90ced97948e9f
edsonlima <edsonlima@edsonlima-Aspire.(none)>
2012-12-08 17:23:24 -0300

    layout inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Escola::Application.config.secret_token = '52cd129d8fda2c1d4e45de9c40a29c5269f3dc85146013401621c3264e3a1904f2574ab8e421b9fd078e52fd615fc6f79c0344af2b930fc2099836efcb6aaa84'
