8a37e5eeb9ef12cdc34d204cded6a95fcbe6d27c
Edson Lima <edsonlima@edson-notebook.(none)>
2013-08-15 15:32:55 -0300

    relatorio de mensalidades no formato pdf

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -1,3 +1,9 @@
+#encoding: utf-8
+require 'prawn'
+require 'prawn/table' 
+require 'prawn/core'
+require 'prawn/security'
+require 'prawn/layout'
 class RelatoriosController < ApplicationController
   def mensalidades_filtro
   	@turmas = current_user.empresa.turmas
@@ -17,7 +23,7 @@ class RelatoriosController < ApplicationController
     mensalidades_filtro = Mensalidade.find(:all, 
     	:conditions => ["vencimento between '#{data_inicial}' and '#{data_final}' #{filtro}"])
     
-    if params[:turma_id]
+    if params[:turma_id] != ""
     	@mensalidades = Array.new
     	mensalidades_filtro.each do |mensalidade|
     		if mensalidade.aluno.turma == Turma.find(params[:turma_id])
@@ -26,7 +32,41 @@ class RelatoriosController < ApplicationController
     	end
     else
 	    @mensalidades = mensalidades_filtro
-	end 
-    #abort @mensalidades
+	end
+	if params[:formato] == "pdf"
+		pdf = mesalidades_pdf @mensalidades
+	    send_data pdf.render, :filename => "mensalidades.pdf", :type => "application/pdf", :disposition => 'inline'
+	end
+  end
+
+  private
+
+  def mesalidades_pdf mensalidades
+  	items = Array.new
+	pdf  = Prawn::Document.new(:page_size => 'A4', :layout => 'portrait') do |pdf|
+      pdf.font_size 14
+      pdf.text "#{current_user.empresa.descricao}"
+      pdf.text "Mensalidades em #{Time.now.strftime('%d/%m/%Y')}"
+      pdf.font_size 10
+     
+      total = 0
+      items << ["Aluno", "Turma", "Vencimento","Quitação", "Atraso", "Valor"]
+      mensalidades.each do |mensalidade|
+      	total = total + mensalidade.valor
+      	items << [
+      		mensalidade.aluno.nome,
+      		mensalidade.aluno.turma.descricao,
+      		mensalidade.vencimento.strftime("%d/%m/%Y"),
+      		mensalidade.data_quitacao ? mensalidade.data_quitacao.strftime("%d/%m/%Y") : "",
+      		mensalidade.data_quitacao ? "" : mensalidade.diasAtraso,
+      		mensalidade.valor
+      	]
+      end
+
+      pdf.table items, :row_colors => ["FFFFFF", "DDDDDD"], :header => true
+      pdf.move_down 10
+ 	  pdf.text "Total ---: #{total}", :style => :bold, :align => :center     	
+    end
+	return pdf    
   end
 end
